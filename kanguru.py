f7a34784a63262d26f453dd5de53c7d4f447d609
nopechy <52882700+nopechy@users.noreply.github.com>
2019-07-27 23:38:48 +0300

    Create kanguru.py

---
+++ kanguru.py
@@ -0,0 +1,3 @@
+print("Merhaba! Size 2 tane kanguru verildi. Bu 2 kangurunun başlangıç noktalarını ve hızlarını yazıcaksınız.")
+print("Ben de bu 2 kangurunun aynı noktaya gelip gelmeme şansını hesaplayacağım.")
+x1,v1,x2,v2=input("Önce 1., sonra 2. kangurunun başlangıç noktasını ve hızını giriniz. (Örnek: 0 3 2 2):").split()
